16022918fd2a46bca823a9411e401c5e0a95cfb6
MarcelCerdan <mthibaul@z3r6p6.42lyon.fr>
2023-12-14 21:40:44 +0100

    Fix norm in display

--- src/display/player.c
+++ src/display/player.c
@@ -12,32 +12,32 @@
 
 #include "cub3D.h"
 
-void    print_player(t_cub *cub);
+void	print_player(t_cub *cub);
 void	init_player(t_cub *cub);
 void	find_angle(t_cub *cub);
 
-void    find_player(t_cub *cub)
+void	find_player(t_cub *cub)
 {
-    int 		i;
-    int 		j;
+	int	i;
+	int	j;
 
-    i = -1;
+	i = -1;
 	init_player(cub);
-    while (cub->parsing.map[++i] && cub->player.x < 0)
-    {
-        j = -1;
-        while (cub->parsing.map[i][++j] && cub->player.x < 0)
-        {
-            if (cub->parsing.map[i][j] == 'N' || cub->parsing.map[i][j] == 'S'
-				|| cub->parsing.map[i][j] == 'E' || cub->parsing.map[i][j] == 'W')
+	while (cub->parsing.map[++i] && cub->player.x < 0)
+	{
+		j = -1;
+		while (cub->parsing.map[i][++j] && cub->player.x < 0)
+		{
+			if (cub->parsing.map[i][j] == 'N' || cub->parsing.map[i][j] == 'S'
+			|| cub->parsing.map[i][j] == 'E' || cub->parsing.map[i][j] == 'W')
 			{
 				cub->player.x = j;
 				cub->player.y = i;
 				cub->player.side = cub->parsing.map[i][j];
 				find_angle(cub);
 			}
-        }
-    }
+		}
+	}
 	cub->player.x = (cub->player.x * WALLS_SIZE) + (WALLS_SIZE / 2);
 	cub->player.y = (cub->player.y * WALLS_SIZE) + (WALLS_SIZE / 2);
 }
