3552f047a3643b3c75d2d2187fb83c8f173cbf25
MarcelCerdan <mthibaul@z3r6p6.42lyon.fr>
2023-12-14 21:56:46 +0100

    Fix norm in header

--- inc/cub3D.h
+++ inc/cub3D.h
@@ -34,29 +34,16 @@
 # define RES_WIDTH 1920
 # define RES_HEIGHT 1080
 # define MAP_WIDTH 800
-# define MAP_HEIGHT 800
 # define WIN_TITLE "Cub3D"
 
-/* --- TEXTURES SIDES --- */
-# define NO 1
-# define SO 2
-# define EA 3
-# define WE 4
-
 /* --- DISPLAY CONSTANTS --- */
-# define ZOOM 20.0
-# define PLAYER_SIZE 11.0
 # define WALLS_SIZE 64.0
 # define FOV 60.0
-# define PLAYER_HEIGHT (WALLS_SIZE / 2.0)
 # define SPEED_Y 0.5
 # define SPEED_X 0.2
 # define SPRINT_SPEED 2
 # define ROTATE_SENS 2
-# define SPEED_SPRINT 12.5
 # define MAP_OFFSET 20
-# define MAP_LIMIT_X (MAP_HEIGHT / 2.0)
-# define MAP_LIMIT_Y (MAP_WIDTH / 2.0)
 
 /* --- PIXEL COLORS --- */
 # define WHITE 0xFFFFFF
@@ -84,14 +71,6 @@
 # define DOWN 65364
 # define SHIFT 65505
 
-/* --- COLORS --- */
-# define K_NORMAL "\x1B[0m"
-# define K_RED "\x1B[31m"
-# define K_GREEN "\x1B[32m"
-# define K_YELLOW "\x1B[33m"
-# define K_BLUE "\x1B[34m"
-# define K_WHITE "\x1B[37m"
-
 /* --- ERROR CODES --- */
 # define K_ERR_MALLOC 12
 
@@ -117,27 +96,21 @@ There's a wrong identifier in the configuration file.\n\x1B[0m"
 # define MSG_ERR_CFG "Error \x1B[0m\n\x1B[33m\
 There's too much information on a line of your configuration file.\n\x1B[0m"
 # define MSG_ERR_COLOUR "Error \x1B[0m\n\x1B[33m\
-An RGB value is incorrect, be sure to input 3 values each between 0 and 255.\n\x1B[0m"
+An RGB value is incorrect, be sure to input \
+3 values each between 0 and 255.\n\x1B[0m"
 # define MSG_ERR_TEXTURE_FILE "Error \x1B[0m\n\x1B[33m\
-A texture file can't be opened, make sure to enter the right map file path.\n\x1B[0m"
+A texture file can't be opened, make sure \
+to enter the right map file path.\n\x1B[0m"
 # define MSG_ERR_WRONG_EXT "A non .cub file was entered. \x1B[0m\n\x1B[33mPlease\
  make sure to only enter .cub files.\n\x1B[0m"
 # define MSG_ERR_ILLEGAL_CHARS_MAP "Illegal characters were found in the map file. \x1B[0m\n\x1B[33mPlease\
  make sure to enter a valid map.\n\x1B[0m"
 # define MSG_ERR_MAP_NOT_CLOSED "The map entered is not closed by walls. \x1B[0m\n\x1B[33mPlease\
  make sure to enter a valid map.\n\x1B[0m"
-# define MSG_ERR_END_OF_LINE_IN_MAP "The map entered is not closed by walls. \x1B[0m\n\x1B[33mPlease\
- make sure to enter a valid map.\n\x1B[0m"
 # define MSG_ERR_EMPTY_LINE "The map has an empty line. \x1B[0m\n\x1B[33mPlease\
  make sure to enter a valid map.\n\x1B[0m"
-# define MSG_ERR_NO_SEP_CFG_MAP "There is no new line between the configuration part and the map. \x1B[0m\n\x1B[33mPlease\
- make sure to have an empty line between the configuration part and the map.\n\x1B[0m"
 # define MSG_ERR_MULTIPLE_SPAWNS "Multiple spawn locations were found. \x1B[0m\n\x1B[33mPlease\
  make sure to only enter one spawn location.\n\x1B[0m"
-# define MSG_ERR_NO_SPAWN "No spawn location was found. \x1B[0m\n\x1B[33mPlease\
- make sure to enter a spawn location.\n\x1B[0m"
-# define MSG_ERR_MLX_INIT "Mlx initialisation has failed. \x1B[0m\n\x1B[33mPlease\
- make sure it is correctly installed.\n\x1B[0m"
 # define MSG_ERR_CREATE_WIN "Window creation has failed. \x1B[0m\n\x1B[33mPlease\
  make sure all drivers are correctly installed.\n\x1B[0m"
 
@@ -154,11 +127,11 @@ typedef struct s_parsing
 	int		floor_color_rgb[3];
 	int		ceiling_color_rgb[3];
 	char	**map;
-    int     map_max_x;
-    int     map_max_y;
-	char 	**cfg;
+	int		map_max_x;
+	int		map_max_y;
+	char	**cfg;
 	char	**file;
-}			t_parsing;
+}	t_parsing;
 
 typedef struct s_mlx
 {
@@ -166,7 +139,7 @@ typedef struct s_mlx
 	void	*win;
 }			t_mlx;
 
-typedef struct	s_img
+typedef struct s_img
 {
 	void	*img;
 	char	*addr;
@@ -178,29 +151,27 @@ typedef struct	s_img
 typedef struct s_player
 {
 	double	x;
-	double 	y;
-	double	new_x;
-	double	new_y;
-	double 	dir;
-	char 	side;
-	int 	horizontal;
-	int 	vertical;
-	int		rotating;
+	double	y;
+	double	dir;
+	char	side;
 	int		sprint;
 	int		forwards;
 	int		backwards;
 	int		left;
 	int		right;
+	int		rotating;
 	int		rotating_left;
 	int		rotating_right;
 	int		map_x;
 	int		map_y;
+	int		horizontal;
+	int		vertical;
 }			t_player;
 
 typedef struct s_point
 {
-	double x;
-	double y;
+	double	x;
+	double	y;
 }	t_point;
 
 typedef struct s_vect
@@ -219,27 +190,27 @@ typedef struct s_ray
 
 typedef struct s_texture
 {
-    void    *mlx;
-    t_img	img;
-    int     width;
-    int     height;
-}   t_texture;
+	void	*mlx;
+	t_img	img;
+	int		width;
+	int		height;
+}	t_texture;
 
 typedef struct s_cub
 {
 	int			is_colision;
-    t_parsing	parsing;
-    t_mlx		mlx;
+	t_parsing	parsing;
+	t_mlx		mlx;
 	int			map_ratio;
-    t_texture   no;
-    t_texture   so;
-    t_texture   ea;
-    t_texture   we;
-    t_img       img;
+	t_texture	no;
+	t_texture	so;
+	t_texture	ea;
+	t_texture	we;
+	t_img		img;
 	t_img		img_mapbg;
 	t_player	player;
 	t_ray		ray;
-}				t_cub;
+}	t_cub;
 
 typedef struct s_rect
 {
@@ -255,21 +226,21 @@ typedef struct s_rect
 
 typedef struct s_displayed_col
 {
-    int         x;
-    int         y;
-    t_texture   *texture;
-}   t_displayed_col;
+	int			x;
+	int			y;
+	t_texture	*texture;
+}	t_displayed_col;
 
 typedef struct s_ray_data
 {
 	double	angle;
-	int 	nmb;
+	int		nmb;
 }	t_ray_data;
 
 typedef struct s_intersection
 {
 	t_point	coord;
-	char 	side;
+	char	side;
 }	t_intersection;
 
 /* --- MAIN FUNCTIONS --- */
@@ -280,11 +251,11 @@ int		parsing(t_cub *cub, int argc, char **argv);
 int		parsing_file(t_cub *cub);
 int		parse_cfg(t_cub *cub);
 void	get_textures(char *dir, char *cfg, t_cub *cub);
-int 	get_colors(char *side, char *cfg, t_cub *cub);
+int		get_colors(char *side, char *cfg, t_cub *cub);
 int		file_exists_check(char *file);
 void	get_cfg(t_cub *cub);
-void	get_map(t_cub *cub, int	i);
-void    map_max_len(t_cub *cub);
+void	get_map(t_cub *cub, int i);
+void	map_max_len(t_cub *cub);
 
 /* --- MAP PARSING --- */
 int		parsing_map(t_cub *cub);
@@ -295,7 +266,7 @@ void	check_diagonals(t_cub *cub);
 /* --- MLX --- */
 int		mlx_main(t_cub *cub);
 void	my_mlx_pixel_put(t_img *img, int x, int y, int color);
-int     mlx_hooks(t_cub *cub);
+int		mlx_hooks(t_cub *cub);
 
 /* --- SHAPES --- */
 void	print_rect(t_img *img, t_rect *rect);
@@ -305,18 +276,21 @@ int		print_map(t_cub *cub);
 void	print_mapbg(t_cub *cub);
 void	find_player(t_cub *cub);
 void	print_player(t_cub *cub);
-void    textures_to_img(t_cub *cub);
+void	textures_to_img(t_cub *cub);
 char	*get_mlx_pxl(t_img *img, int x, int y);
 
 /* --- RAY CASTING --- */
 int		ray_casting_main(t_cub *cub);
 void	init_ray_cast(t_cub *cub);
 double	calc_wall_distance(t_cub *cub, t_point inter);
-int		draw_walls(t_cub *cub, double wall_dist, int ray_nmb, t_displayed_col *displayed_col);
-int     move_player_3d(t_cub *cub);
-void	find_smallest_dist(t_cub *cub, t_point walls, t_displayed_col *displayed_col, t_ray_data ray);
+int		draw_walls(t_cub *cub, double wall_dist, \
+		int ray_nmb, t_displayed_col *displayed_col);
+int		move_player_3d(t_cub *cub);
+void	find_smallest_dist(t_cub *cub, t_point walls, \
+		t_displayed_col *displayed_col, t_ray_data ray);
 int		check_wall(t_cub *cub, t_point inter);
-double	increment_ray(t_cub *cub, t_intersection *inter, t_point a, int *int_coord);
+double	increment_ray(t_cub *cub, t_intersection *inter, \
+		t_point a, int *int_coord);
 int		check_error_in_ray(t_cub *cub, t_intersection inter);
 void	collision_animation(t_cub *cub);
 
